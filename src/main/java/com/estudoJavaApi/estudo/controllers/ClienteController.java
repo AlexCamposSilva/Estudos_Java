2aa5c91784f141cab3fa13d84355053b5c511bb9
Alex Campos <alexcampostec@gmail.com>
2024-05-01 21:16:37 -0300

    Inserindo Put

--- src/main/java/com/estudoJavaApi/estudo/controllers/ClienteController.java
+++ src/main/java/com/estudoJavaApi/estudo/controllers/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +42,18 @@ public class ClienteController {
 		newCliente.setCpf(cliente.getCpf());
 		
 		return ResponseEntity.ok().body(clienteRepositories.save(newCliente));	
-		}
-	
+	}
+
+
+	@PutMapping("/clientes/{id}")
+	private ResponseEntity<Cliente> atualizar(@PathVariable (value = "id") UUID id,
+										      @RequestBody Cliente cliente){
+		Optional<Cliente> obj = clienteRepositories.findById(id);
+		var newcliente = obj.get();
+		newcliente.setName(cliente.getName());
+		newcliente.setCpf(cliente.getCpf());
+		
+		return ResponseEntity.ok().body(clienteRepositories.save(newcliente));
+	}
+
 }
